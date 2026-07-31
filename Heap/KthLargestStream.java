5443675f7e68daba6fe63a6bb87aa555d42d548d
Shaurya <shauryakamal117@gmail.com>
2026-07-31 22:42:18 +0530

    Added and solved leetCode question 703

---
+++ Heap/KthLargestStream.java
@@ -0,0 +1,37 @@
+package Heap;
+
+import java.util.PriorityQueue;
+
+public class KthLargestStream {
+
+    static class KthLargest {
+        private PriorityQueue<Integer> minHeap;
+        private int k;
+
+        public KthLargest(int k, int[] nums) {
+            this.k = k;
+            minHeap = new PriorityQueue<>();
+            for (int num : nums) {
+                add(num);
+            }
+        }
+
+        public int add(int val) {
+            minHeap.offer(val);
+            if (minHeap.size() > k) {
+                minHeap.poll();
+            }
+            return minHeap.peek();
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {4, 5, 8, 2};
+        KthLargest kthLargest = new KthLargest(3, nums);
+        System.out.println(kthLargest.add(3));
+        System.out.println(kthLargest.add(5));
+        System.out.println(kthLargest.add(10));
+        System.out.println(kthLargest.add(9));
+        System.out.println(kthLargest.add(4));
+    }
+}
