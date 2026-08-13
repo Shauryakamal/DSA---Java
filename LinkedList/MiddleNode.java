5236eb7afdf7a392f579223e57a68cffe5c1814a
Shaurya <shauryakamal117@gmail.com>
2026-08-13 11:21:30 +0530

    Added LeetCode question 19 and 876

---
+++ LinkedList/MiddleNode.java
@@ -0,0 +1,49 @@
+package LinkedList;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class MiddleNode {
+
+    public ListNode middleNode(ListNode head) {
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+    public static void main(String[] args) {
+
+        MiddleNode obj = new MiddleNode();
+
+        // 1 → 2 → 3 → 4
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+
+        ListNode result = obj.middleNode(head);
+
+        System.out.println(result.val);
+    }
+}
